fb5f221d9b3732fe0df6d353e11c26af5c0e5fd9
radmilak <radmilko@ifi.uio.no>
2020-03-27 14:02:37 +0100

    Update setup.py
    remove pygraphviz

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     package_data={"pytest_html_profiling": ["resources/*"]},
     entry_points={"pytest11": ["html = pytest_html_profiling.profiling_plugin"]},
     setup_requires=["setuptools_scm"],
-    install_requires=["pytest>=3.0", "pytest-metadata", 'pygraphviz', 'gprof2dot'],
+    install_requires=["pytest>=3.0", "pytest-metadata", 'gprof2dot'],
     license="Mozilla Public License 2.0 (MPL 2.0)",
     keywords="py.test pytest html report",
     classifiers=[
